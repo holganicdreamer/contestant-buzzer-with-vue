61f437c09ffa5bdd4a700af5266c67cece65279c
moviewhisper <holganicbear@gmail.com>
2019-12-31 15:48:43 +0900

    初期登録

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div @click="click">
+    <div class="box">
+      <img :src="imageSrc">
+      <h2 v-if="displayDate">あなたの押した時刻は {{displayDate}} です。</h2>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Howl } from 'howler'
+import moment from 'moment'
+import RedButton from "../assets/red_button.png"
+import GreenButton from "../assets/green_button.png"
+import Buzzer1 from "../assets/buzzer1.mp3"
+import Buzzer2 from "../assets/buzzer2.mp3"
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  data: function() {
+    return {
+      audio: null,
+      date: null,
+      opt: this.$route.params.param1,
+      imageSrc: null,
+    }
+  },
+  methods: {
+    click() {
+      // console.log("clicked")
+      this.date = moment()
+      this.audio.play()
+    }
+  },
+  computed: {
+    displayDate: function() {
+      if(this.date == null) {
+        return null;
+      }
+      else {
+        return this.date.format('HH:mm:ss.SS');
+      }
+    }
+  },
+  mounted () {
+    if(this.$route.params.param1 == 2) {
+      this.imageSrc = GreenButton
+      // this.audio = new Howl({ src: ["https://otologic.jp/sounds/se/pre/Quiz-Buzzer01-1.mp3"] })
+      this.audio = new Howl({ src: [Buzzer2] })
+    }
+    else {
+      this.imageSrc = RedButton
+      // this.audio = new Howl({ src: ["https://otologic.jp/sounds/se/pre/Quiz-Buzzer02-1.mp3"] })
+      this.audio = new Howl({ src: [Buzzer1] })
+    }
+    
+  },
 }
 </script>
+
+<style>
+.box {
+  font-family: 'Lato', 'Noto Sans JP', '游ゴシック Medium', '游ゴシック体', 'Yu Gothic Medium', YuGothic, 'ヒラギノ角ゴ ProN', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
+  /* background-color: pink; */
+  text-align: center;
+  height: 100vh;
+}
+.box > h2 {
+  font-size: 1.5rem;
+}
+@media screen and (orientation: landscape) {
+  .box > img {
+    width: 80vh;
+  }
+}
+@media screen and (orientation: portrait) {
+  .box > img {
+    width: 80vw;
+  }
+}
+</style>
